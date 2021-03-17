989c468cbb9231f9d19bc54788ba9d5845e32b34
Mason <jeailfy9520@outlook.com>
2021-03-17 08:38:06 +0800

    feat: timer

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,10 +1,41 @@
+import { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import * as actionCreators from '../store/actionCreators';
 import { TimerWrapper, Counter } from './style';
 
 
 const Timer = () => {
+  const [m, setM] = useState(0);
+  const [s, setS] = useState(0);
+
+  const inputRef = useRef(0);
+  const minuteRef = useRef(0);
+  const secondRef = useRef(0);
   const dispatch = useDispatch();
+
+  const setTimer = () => {
+    const inputVal = inputRef.current.value;
+    setM(inputVal);
+    minuteRef.current = inputVal;
+    let x = setInterval(() => {
+      if (secondRef.current === 0) {
+        setM(prev => prev - 1);
+        setS(prev => prev === 0 ? 59 : prev);
+        minuteRef.current -= 1;
+        secondRef.current = 59;
+      } else {
+        setS(prev => prev - 1);
+        secondRef.current -= 1;
+      }
+
+      if (secondRef.current === 0 && minuteRef.current === 0) {
+        clearInterval(x);
+        setM(0);
+        setS(0);
+        openModal();
+      }
+    }, 1000);
+  }
 
   const openModal = () => {
     const random = Math.floor(Math.random() * 10);
@@ -15,10 +46,10 @@ const Timer = () => {
   return (
     <TimerWrapper>
       <h2>抽獎時間</h2>
-      <input type="text" />
+      <input type="number" min='0' ref={inputRef} />
       <span>分鐘</span>
-      <button onClick={openModal}>設定</button>
-      <Counter>00:00</Counter>
+      <button onClick={setTimer}>設定</button>
+      <Counter>{m}:{s}</Counter>
     </TimerWrapper>
   );
 }
