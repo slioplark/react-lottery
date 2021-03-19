fc204b40c366bf2551de64750db2da853b56f1b0
Mason <jeailfy9520@outlook.com>
2021-03-19 13:54:44 +0800

    fix: timer

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,61 +1,68 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import * as actionCreators from '../store/actionCreators';
 import { TimerWrapper, Counter } from './style';
 
-
 const Timer = () => {
-  const [m, setM] = useState(0);
-  const [s, setS] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [isReset, setIsReset] = useState(false);
 
   const inputRef = useRef(0);
   const minuteRef = useRef(0);
   const secondRef = useRef(0);
   const dispatch = useDispatch();
 
-  const setTimer = () => {
+  useEffect(() => {
     let timer = null;
-    const inputVal = Number(inputRef.current.value) || 0;
+    let inputVal = Number(inputRef.current.value) || 0;
+    if (inputVal < 0) inputVal = 0;
 
-    if (!inputVal) return;
+    const openModal = () => {
+      const random = Math.floor(Math.random() * 10);
+      dispatch(actionCreators.getUser(random));
+      dispatch(actionCreators.openModal());
+    }
 
-    setM(inputVal);
+    setMinute(inputVal);
+    setSecond(0);
     minuteRef.current = inputVal;
+    secondRef.current = 0;
 
-    clearInterval(timer);
-    timer = setInterval(() => {
-      if (secondRef.current === 0) {
-        setM(prev => prev - 1);
-        setS(prev => prev === 0 ? 59 : prev);
-        minuteRef.current -= 1;
-        secondRef.current = 59;
-      } else {
-        setS(prev => prev - 1);
-        secondRef.current -= 1;
-      }
+    if (inputVal > 0) {
+      timer = setInterval(() => {
+        if (secondRef.current === 0) {
+          setMinute(prev => prev - 1);
+          setSecond(prev => prev === 0 ? 59 : prev);
+          minuteRef.current -= 1;
+          secondRef.current = 59;
+        } else {
+          setSecond(prev => prev - 1);
+          secondRef.current -= 1;
+        }
 
-      if (secondRef.current === 0 && minuteRef.current === 0) {
-        clearInterval(timer);
-        setM(0);
-        setS(0);
-        openModal();
-      }
-    }, 1000);
-  }
+        if (secondRef.current === 0 && minuteRef.current === 0) {
+          clearInterval(timer);
+          setMinute(0);
+          setSecond(0);
+          openModal();
+        }
+      }, 1000);
+    }
 
-  const openModal = () => {
-    const random = Math.floor(Math.random() * 10);
-    dispatch(actionCreators.getUser(random));
-    dispatch(actionCreators.openModal());
-  }
+    return () => {
+      clearInterval(timer);
+      setIsReset(false);
+    }
+  }, [isReset, dispatch]);
 
   return (
     <TimerWrapper>
       <h2>抽獎時間</h2>
       <input type="number" min='0' ref={inputRef} />
       <span>分鐘</span>
-      <button onClick={setTimer}>設定</button>
-      <Counter>{m}:{s}</Counter>
+      <button onClick={() => setIsReset(true)}>設定</button>
+      <Counter>{minute}:{second}</Counter>
     </TimerWrapper>
   );
 }
